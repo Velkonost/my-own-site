75442f6f688c7beee0cb44a365f379d5372c1dec
Artem <klimenkocpp@icloud.com>
2017-06-22 13:49:14 +0600

    main page completed

---
+++ js/common.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+
+	$(".section_header").animated("fadeInUp", "fadeOutDown");
+	//$(".dopInformation").animated("fadeInUp", "fadeOutDown");
+
+	$(".animation_1").animated("flipInY", "fadeOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	$(".animation_3").animated("fadeInRight", "fadeOutDown");
+
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
+
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a.seework").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
+	});
+
+	$("input, select, textarea").jqBootstrapValidation();
+
+	// $(".top_mnu ul a").mPageScroll2id();
+
+});
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".top_text p").animated("fadeInUp", "fadeOutDown");
+
+}); 
+
+ $(document).ready(function() {
+
+    //Calculate the height of <header>
+    //Use outerHeight() instead of height() if have padding
+    var aboveHeight = $('header').outerHeight() + $('section#about').outerHeight();
+
+//when scroll
+    $(window).scroll(function(){
+
+        //if scrolled down more than the header’s height
+            if ($(window).scrollTop() > aboveHeight){
+
+        // if yes, add “fixed” class to the <nav>
+        // add padding top to the #content 
+            
+            $('#nav').addClass('fixed').css('top','0');
+            
+            // document.getElementById('main').css('padding-top','60px');
+
+            } else {
+
+        // when scroll up or less than aboveHeight,
+            
+            $('#nav').removeClass('fixed');
+            // document.getElementById('main').css('padding-top','0');
+            }
+        });
+    });
+
+    $("a[href='#top']").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
